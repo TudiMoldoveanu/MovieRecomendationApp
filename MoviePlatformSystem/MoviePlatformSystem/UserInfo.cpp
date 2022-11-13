c01cae2e9fcd88e5a282bd5ac6b40cf57d9ef3b6
TudiMoldoveanu <tudimoldoveanu@gmail.com>
2022-11-13 09:37:21 +0200

    Implemented the get and set functions in the UserInfo and UserPreferences cpps.

--- MoviePlatformSystem/MoviePlatformSystem/UserInfo.cpp
+++ MoviePlatformSystem/MoviePlatformSystem/UserInfo.cpp
@@ -1,1 +1,42 @@
 #include "UserInfo.h"
+
+UserInfo::UserInfo()
+{
+}
+
+UserInfo::UserInfo(std::string id, std::string fullName, std::string age)
+	:m_id{ id }, m_fullName{ fullName }, m_age{ age } {}
+
+UserInfo::~UserInfo()
+{
+}
+
+void UserInfo::setId(const std::string& id)
+{
+	m_id = id;
+}
+
+void UserInfo::setFullName(const std::string& fullName)
+{
+	m_fullName = fullName;
+}
+
+void UserInfo::setAge(const std::string& age)
+{
+	m_age = age;
+}
+
+std::string UserInfo::getId() const
+{
+	return m_id;
+}
+
+std::string UserInfo::getFullName() const
+{
+	return m_fullName;
+}
+
+std::string UserInfo::getAge() const
+{
+	return m_age;
+}
--- MoviePlatformSystem/MoviePlatformSystem/UserPreferences.cpp
+++ MoviePlatformSystem/MoviePlatformSystem/UserPreferences.cpp
@@ -1,1 +1,37 @@
 #include "UserPreferences.h"
+
+UserPreferences::UserPreferences()
+{
+
+}
+
+UserPreferences::UserPreferences(std::string id, std::string showId) 
+	:m_id{ id }, m_showId{ showId } {}
+
+UserPreferences::~UserPreferences()
+{
+}
+
+void UserPreferences::setId(const std::string& id)
+{
+	m_id = id;
+}
+
+void UserPreferences::setShowId(const std::string& showId)
+{
+	m_showId = std::move(showId);
+}
+
+std::string UserPreferences::getId() const
+{
+	return m_id;
+}
+
+std::string UserPreferences::getShowId() const
+{
+	return m_showId;
+}
+
+
+
+
