3ae63f113b16edf5423c2ed866bd3da3300182c9
Gabiichim-unitbv <gabiichim230@yahoo.com>
2022-11-18 21:47:18 +0200

    Added the foreign keys, added the primary composite key where needed, added a little insert test and commented the previous tests.

--- MoviePlatformSystem/MoviePlatformSystem/Source.cpp
+++ MoviePlatformSystem/MoviePlatformSystem/Source.cpp
@@ -14,291 +14,250 @@
 
 
 
-int main(int, char**)
+int main(int, char** argv)
+
 {
 	namespace sql = sqlite_orm;
 
-	//auto platformDatabase = sql::make_storage(
-	//	"moviePlatform.sqlite",
-	//	sql::make_table
-	//	(
-	//		"movies",
-	//		sql::make_column
-	//		(
-	//			"show_id",
-	//			&Movie::getId,
-	//			&Movie::setId,
-	//			sql::primary_key()
-	//		),
-	//		sql::make_column
-	//		(
-	//			"type",
-	//			&Movie::getType,
-	//			&Movie::setType
-	//		),
-	//		sql::make_column
-	//		(
-	//			"title",
-	//			&Movie::getTitle,
-	//			&Movie::setTitle
-	//		),
-	//		sql::make_column
-	//		(
-	//			"director",
-	//			&Movie::getDirector,
-	//			&Movie::setDirector
-	//		),
-	//		sql::make_column
-	//		(
-	//			"cast",
-	//			&Movie::getCast,
-	//			&Movie::setCast
-	//		),
-	//		sql::make_column
-	//		(
-	//			"country",
-	//			&Movie::getCountry,
-	//			&Movie::setCountry
-	//		),
-	//		sql::make_column
-	//		(
-	//			"date_added",
-	//			&Movie::getDateAdded,
-	//			&Movie::setDateAdded
-	//		),
-	//		sql::make_column
-	//		(
-	//			"release_year",
-	//			&Movie::getReleaseYear,
-	//			&Movie::setReleaseYear
-	//		),
-	//		sql::make_column
-	//		(
-	//			"rating",
-	//			&Movie::getRating,
-	//			&Movie::setRating
-	//		),
-	//		sql::make_column
-	//		(
-	//			"duration",
-	//			&Movie::getDuration,
-	//			&Movie::setDuration
-	//		),
-	//		sql::make_column
-	//		(
-	//			"listed_in",
-	//			&Movie::getListedIn,
-	//			&Movie::setListedIn
-	//		),
-	//		sql::make_column
-	//		(
-	//			"description",
-	//			&Movie::getDescription,
-	//			&Movie::setDescription
-	//		)
-	//	),
-	//	sql::make_table
-	//	(
-	//		"users",
-	//		sql::make_column
-	//		(
-	//			"user_id",
-	//			&User::getId,
-	//			&User::setId,
-	//			sql::primary_key()
-	//		),
-	//		sql::make_column
-	//		(
-	//			"username",
-	//			&User::getUsername,
-	//			&User::setUsername
-	//		),
-	//		sql::make_column
-	//		(
-	//			"password",
-	//			&User::getPassword,
-	//			&User::setPassword
-	//		),
-	//		sql::make_column(
-	//			"user_info_id", // fk
-	//			&User::getUserInfoId,
-	//			&User::setUserInfoId
-	//		),
-	//		sql::make_column
-	//		(
-	//			"user_preferences_id", // fk
-	//			&User::getUserPreferencesId,
-	//			&User::setUserPreferencesId
-	//		),
-	//		sql::make_column
-	//		(
-	//			"user_watched_id", // fk
-	//			&User::getWatchedId,
-	//			&User::setWatchedId
+	auto platformDatabase = sql::make_storage(
+		"moviePlatform.sqlite",
+		sql::make_table
+		(
+			"movies",
+			sql::make_column
+			(
+				"show_id",
+				&Movie::getId,
+				&Movie::setId,
+				sql::primary_key()
+			),
+			sql::make_column
+			(
+				"type",
+				&Movie::getType,
+				&Movie::setType
+			),
+			sql::make_column
+			(
+				"title",
+				&Movie::getTitle,
+				&Movie::setTitle
+			),
+			sql::make_column
+			(
+				"director",
+				&Movie::getDirector,
+				&Movie::setDirector
+			),
+			sql::make_column
+			(
+				"cast",
+				&Movie::getCast,
+				&Movie::setCast
+			),
+			sql::make_column
+			(
+				"country",
+				&Movie::getCountry,
+				&Movie::setCountry
+			),
+			sql::make_column
+			(
+				"date_added",
+				&Movie::getDateAdded,
+				&Movie::setDateAdded
+			),
+			sql::make_column
+			(
+				"release_year",
+				&Movie::getReleaseYear,
+				&Movie::setReleaseYear
+			),
+			sql::make_column
+			(
+				"rating",
+				&Movie::getRating,
+				&Movie::setRating
+			),
+			sql::make_column
+			(
+				"duration",
+				&Movie::getDuration,
+				&Movie::setDuration
+			),
+			sql::make_column
+			(
+				"listed_in",
+				&Movie::getListedIn,
+				&Movie::setListedIn
+			),
+			sql::make_column
+			(
+				"description",
+				&Movie::getDescription,
+				&Movie::setDescription
+			)
+		),
+		sql::make_table
+		(
+			"users",
+			sql::make_column
+			(
+				"user_id",
+				&User::getId,
+				&User::setId,
+				sql::primary_key(),
+				sql::autoincrement()
+			),
+			sql::make_column
+			(
+				"username",
+				&User::getUsername,
+				&User::setUsername
+			),
+			sql::make_column
+			(
+				"password",
+				&User::getPassword,
+				&User::setPassword
+			)
+		),
+		sql::make_table
+		(
+			"user_preferences",
+			sql::make_column
+			(
+				"user_id", // fk
+				&UserPreferences::getUserId,
+				&UserPreferences::setUserId
+			),
+			sql::make_column
+			(
+				"show_id", // fk
+				&UserPreferences::getShowId,
+				&UserPreferences::setShowId
+			),
+			sql::primary_key(&UserPreferences::setUserId, &UserPreferences::setShowId),
+			sql::foreign_key(&UserPreferences::setUserId).references(&User::setId),
+			sql::foreign_key(&UserPreferences::setShowId).references(&Movie::setId)
+		),
+		sql::make_table
+		(
+			"user_info",
+			sql::make_column
+			(
+				"user_id", // fk
+				&UserInfo::getUserId,
+				&UserInfo::setUserId
+			),
 
-	//		),
-	//		sql::make_column
-	//		(
-	//			"user_wishlist_id", // fk
-	//			&User::getWishlistId,
-	//			&User::setWishlistId
+			sql::make_column
+			(
+				"fullname",
+				&UserInfo::getFullName,
+				&UserInfo::setFullName
+			),
+			sql::make_column
+			(
+				"age",
+				&UserInfo::getAge,
+				&UserInfo::setAge
+			),
+			sql::primary_key(&UserInfo::getUserId),
+			sql::foreign_key(&UserInfo::setUserId).references(&User::setId)
 
-	//		),
-	//		sql::foreign_key(&User::setUserPreferencesId).references(&UserPreferences::setId) // have to.. 
-	//																		   // ..figure it out why it's not working
-	//	),// ^^ to add the FK
-	//	sql::make_table
-	//	(
-	//		"user_preferences",
-	//		sql::make_column
-	//		(
-	//			"id",
-	//			&UserPreferences::getId,
-	//			&UserPreferences::setId,
-	//			sql::primary_key()
-	//		),
-	//		sql::make_column
-	//		(
-	//			"user_id", // fk
-	//			&UserPreferences::getUserId,
-	//			&UserPreferences::setUserId
-	//		),
+		),
+		sql::make_table
+		(
+			"user_watched",
+			sql::make_column
+			(
+				"user_id", // fk
+				&UserWatched::getUserId,
+				&UserWatched::setUserId
+			),
+			sql::make_column
+			(
+				"show_id", // fk
+				&UserWatched::getShowId,
+				&UserWatched::setShowId
+			),
+			sql::primary_key(&UserWatched::setUserId, &UserWatched::setShowId),
+			sql::foreign_key(&UserWatched::setUserId).references(&User::setId),
+			sql::foreign_key(&UserWatched::setShowId).references(&Movie::setId)
+		),
+		sql::make_table
+		(
+			"user_wishlist",
+			sql::make_column
+			(
+				"user_id", // fk
+				&UserWishlist::getUserId,
+				&UserWishlist::setUserId
+			),
+			sql::make_column
+			(
+				"show_id", // fk
+				&UserWishlist::getShowId,
+				&UserWishlist::setShowId
+			),
+			sql::primary_key(&UserWishlist::setUserId, &UserWishlist::setShowId),
+			sql::foreign_key(&UserWishlist::setUserId).references(&User::setId),
+			sql::foreign_key(&UserWishlist::setShowId).references(&Movie::setId)
+		),
+		sql::make_table
+		(
+			"rating",
+			sql::make_column
+			(
+				"user_id", // fk
+				&Rating::getUserId,
+				&Rating::setUserId
+			),
+			sql::make_column
+			(
+				"show_id", // fk
+				&Rating::getShowId,
+				&Rating::setShowId
+			),
+			sql::make_column
+			(
+				"rating", // fk
+				&Rating::getRating,
+				&Rating::setRating
+			),
+			sql::primary_key(&Rating::setUserId, &Rating::setShowId),
+			sql::foreign_key(&Rating::setUserId).references(&User::setId),
+			sql::foreign_key(&Rating::setShowId).references(&Movie::setId)
+		)
+	);
 
-	//		sql::make_column
-	//		(
-	//			"show_id", // fk
-	//			&UserPreferences::getShowId,
-	//			&UserPreferences::setShowId
-	//		)
-	//	),
-	//	sql::make_table
-	//	(
-	//		"user_info",
-	//		sql::make_column
-	//		(
-	//			"id",
-	//			&UserInfo::getId,
-	//			&UserInfo::setId,
-	//			sql::primary_key()
-	//		),
-	//		sql::make_column
-	//		(
-	//			"user_id", // fk
-	//			&UserInfo::getUserId,
-	//			&UserInfo::setUserId
-	//		),
+	platformDatabase.sync_schema(true);
 
-	//		sql::make_column
-	//		(
-	//			"fullname",
-	//			&UserInfo::getFullName,
-	//			&UserInfo::setFullName
-	//		),
-	//		sql::make_column
-	//		(
-	//			"age",
-	//			&UserInfo::getAge,
-	//			&UserInfo::setAge
-	//		)
-	//	),
-	//	sql::make_table
-	//	(
-	//		"user_watched",
-	//		sql::make_column
-	//		(
-	//			"id",
-	//			&UserWatched::getId,
-	//			&UserWatched::setId,
-	//			sql::primary_key()
-	//		),
-	//		sql::make_column
-	//		(
-	//			"user_id", // fk
-	//			&UserWatched::getUserId,
-	//			&UserWatched::setUserId
-	//		),
-	//		sql::make_column
-	//		(
-	//			"show_id", // fk
-	//			&UserWatched::getShowId,
-	//			&UserWatched::setShowId
-	//		)
-	//	),
-	//	sql::make_table
-	//	(
-	//		"user_wishlist",
-	//		sql::make_column
-	//		(
-	//			"id",
-	//			&UserWishlist::getId,
-	//			&UserWishlist::setId,
-	//			sql::primary_key()
-	//		),
-	//		sql::make_column
-	//		(
-	//			"user_id", // fk
-	//			&UserWishlist::getUserId,
-	//			&UserWishlist::setUserId
-	//		),
-	//		sql::make_column
-	//		(
-	//			"show_id", // fk
-	//			&UserWishlist::getShowId,
-	//			&UserWishlist::setShowId
-	//		)
-	//	),
-	//	sql::make_table
-	//	(
-	//		"rating",
-	//		sql::make_column
-	//		(
-	//			"id",
-	//			&Rating::getId,
-	//			&Rating::setId,
-	//			sql::primary_key()
-	//		),
-	//		sql::make_column
-	//		(
-	//			"user_id", // fk
-	//			&Rating::getUserId,
-	//			&Rating::setUserId
-	//		),
-	//		sql::make_column
-	//		(
-	//			"show_id", // fk
-	//			&Rating::getShowId,
-	//			&Rating::setShowId
-	//		),
-	//		sql::make_column
-	//		(
-	//			"rating", // fk
-	//			&Rating::getRating,
-	//			&Rating::setRating
-	//		)
-	//	)
-	//);
 
-	//// TO ADD THE FKEYS!
-	//platformDatabase.sync_schema(true);
+	// testing..
 
-	//// testing..
+	//PRINT MOVIE DATA
 
-	////PRINT MOVIE DATA
+	//display no. of movies in .prodb
+	std::ofstream file("movies.prodb");
+	auto allMovies = platformDatabase.get_all<Movie>();
+	file << "allMovies (" << allMovies.size() << "):" << std::endl;
 
-	////display no. of movies in .prodb
-	//std::ofstream file("movies.prodb");
-	//auto allMovies = platformDatabase.get_all<Movie>();
-	//file << "allMovies (" << allMovies.size() << "):" << std::endl;
+	//display all movies in .prodb
+	for (auto& movie : platformDatabase.iterate<Movie>()) {
+		file << platformDatabase.dump(movie) << std::endl;
+	}
 
-	////display all movies in .prodb
-	//for (auto& movie : platformDatabase.iterate<Movie>()) {
-	//	file << platformDatabase.dump(movie) << std::endl;
-	//}
+	/*USER TABLE CRUD OPERATIONS*/
 
-	////USER TABLE CRUD OPERATIONS
+	// a little example of insert
+	User user{ -1, "anamaria", "pass" }; // i think the id really doesn't matter bcs we have the autoincrement that starts from 1
+	platformDatabase.insert(user);
+	platformDatabase.replace(UserInfo({ 1, "Ana Maria Prodan", "32" }));
 
-	////insert
-	//platformDatabase.replace(User("1", "user1Name", "password1","1", "1", "1", "1"));
+	// COMMENTED TESTS FROM PREVIOUS STORAGE ARE DOWN BELOW
+
 	//platformDatabase.replace(User("2", "user2Name", "password2", "2", "2", "2", "2"));
 	//platformDatabase.replace(User("3", "user3Name", "password3", "3", "3", "3", "3"));
 	////delete
@@ -317,7 +276,7 @@ int main(int, char**)
 	//std::cout << std::endl;
 
 	////USER INFO TABLE CRUD OPERATIONS
-
+	//
 	////insert
 	//platformDatabase.replace(UserInfo("1", "1", "fullName1", "age1"));
 	//platformDatabase.replace(UserInfo("2", "2", "fullName2", "age2"));
@@ -358,7 +317,7 @@ int main(int, char**)
 	//	std::cout << platformDatabase.dump(userWatched) << std::endl;
 	//}
 
-
+	//
 	////WISHLIST TABLE CRUD OPERATIONS
 	//// 
 	////insert
