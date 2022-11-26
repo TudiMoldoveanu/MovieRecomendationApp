e0550185cf8927e977d6099f01a546eb94aa4aea
zsoltibv <zsoltibv@gmail.com>
2022-11-26 23:38:47 +0200

    Implemented Login functionalities and linked it to login form

---
+++ MoviePlatformApp/MoviePlatformApp/LogInPage.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include <QMessageBox>
+#include "ui_LogInPage.h"
+#include "DataBase.h"
+
+class LogInPage : public QMainWindow, public Database
+{
+    Q_OBJECT
+
+public:
+    LogInPage(QWidget *parent = nullptr);
+    ~LogInPage();
+    
+    bool verifiyLogin(std::string username, std::string password);
+
+private slots:
+    void on_logInButton_clicked();
+
+private:
+    Ui::LogInPage ui;
+};
