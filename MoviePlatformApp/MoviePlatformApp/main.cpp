557531ba43f45a9d628ecb0ccd1e0bcc3c4423a1
TudiMoldoveanu <tudimoldoveanu@gmail.com>
2022-11-26 17:55:23 +0200

    Made a qt application and added a login window. Also transfered the old .ccps and .hs into it.

---
+++ MoviePlatformApp/MoviePlatformApp/main.cpp
@@ -0,0 +1,9 @@
+#include "LogInPage.h"
+#include <QtWidgets/QApplication>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    LogInPage logInPage;
+    logInPage.show(); 
+    return a.exec();
+}
