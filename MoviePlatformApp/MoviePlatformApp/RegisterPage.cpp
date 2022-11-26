e95af2290b587b192e1defa1aa91ba03aaf8abfe
zsoltibv <zsoltibv@gmail.com>
2022-11-26 21:22:24 +0200

    Added Register Form validation with custom messages for weak password, short username, and empty fields.

--- MoviePlatformApp/MoviePlatformApp/RegisterPage.cpp
+++ MoviePlatformApp/MoviePlatformApp/RegisterPage.cpp
@@ -1,8 +1,10 @@
 #include "RegisterPage.h"
 #include "DataBase.h"
 #include <iostream>
+#include <QMessageBox>
+#include <regex>
 
-RegisterPage::RegisterPage(QWidget *parent)
+RegisterPage::RegisterPage(QWidget* parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
@@ -11,17 +13,55 @@ RegisterPage::RegisterPage(QWidget *parent)
 RegisterPage::~RegisterPage()
 {}
 
+bool RegisterPage::validateRegisterForm(std::string username, std::string password, std::string fullname, std::string age)
+{
+	//Minimum eight characters, at least one letter and one number:
+	std::regex pass_rule("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
+	//
+	if (username.empty() || password.empty() || fullname.empty() || age.empty()) {
+		m_errorCode = ErrorCodes::EmptyFields;
+		return false;
+	}
+	if (username.size() < 3) {
+		m_errorCode = ErrorCodes::UsernameSize;
+		return false;
+	}
+	if (regex_match(password, pass_rule)){
+		m_errorCode = ErrorCodes::WeakPassword;
+		return false;
+	}
+	return true;
+}
+
+const char* RegisterPage::errorCodeToString(const ErrorCodes& type)
+{
+	switch (type) {
+	case 0:
+		return "One of the fields is empty!";
+	case 1:
+		return "Username too short.";
+	case 2:
+		return "Password is too weak.(Use at least eight characters, one number and one letter)";
+	default:
+		return "none";
+	}
+}
+
 void RegisterPage::on_pushButton_clicked() {
-	QString username = ui.userLineEdit->text();
-	QString password = ui.passLineEdit->text();
-	QString fullname = ui.nameLineEdit->text();
-	QString age = ui.ageSpinBox->text();
+	std::string username = ui.userLineEdit->text().toStdString();
+	std::string  password = ui.passLineEdit->text().toStdString();
+	std::string  fullname = ui.nameLineEdit->text().toStdString();
+	std::string  age = ui.ageSpinBox->text().toStdString();
 
-	Database *db = new Database;
+	if (validateRegisterForm(username, password, fullname, age)) {
+		User user{ -1, username, password };
+		UserInfo userInfo{ user.getId(), fullname, age };
 
-	User user{ -1, username.toStdString(), password.toStdString() };
-	db->insertIntoUser(user);
-
-	UserInfo userInfo{ user.getId(), fullname.toStdString(), age.toStdString() };
-	db->insertIntoUserInfo(userInfo);
+		Database* db = new Database;
+		db->insertIntoUser(user);
+		db->insertIntoUserInfo(userInfo);
+	}
+	else {
+		QMessageBox::information(this, "Register", errorCodeToString(m_errorCode));
+	}
 }
--- MoviePlatformApp/MoviePlatformApp/RegisterPage.h
+++ MoviePlatformApp/MoviePlatformApp/RegisterPage.h
@@ -3,6 +3,12 @@
 #include <QMainWindow>
 #include "ui_RegisterPage.h"
 
+enum ErrorCodes : uint8_t {
+	EmptyFields,
+	UsernameSize,
+	WeakPassword,
+};
+
 class RegisterPage : public QMainWindow
 {
 	Q_OBJECT
@@ -11,9 +17,15 @@ public:
 	RegisterPage(QWidget *parent = nullptr);
 	~RegisterPage();
 
+	bool validateRegisterForm(std::string username, std::string password,
+		std::string fullname, std::string age);
+
+	const char* errorCodeToString(const ErrorCodes& type);
+
 private slots:
 	void on_pushButton_clicked();
 
 private:
 	Ui::RegisterPageClass ui;
+	ErrorCodes m_errorCode;
 };
