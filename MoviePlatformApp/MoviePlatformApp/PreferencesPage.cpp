16ff7b9761c160b7c5dd165e1f112171773e7fa8
raluca47 <raluca987123@gmail.com>
2022-11-28 01:17:23 +0200

    Implemented PreferencesPage.cpp to setup the preferences page ui

--- MoviePlatformApp/MoviePlatformApp/PreferencesPage.cpp
+++ MoviePlatformApp/MoviePlatformApp/PreferencesPage.cpp
@@ -1,1 +1,10 @@
 #include "PreferencesPage.h"
+
+PreferencesPage::PreferencesPage(QWidget* parent)
+    : QMainWindow(parent)
+{
+
+    ui.setupUi(this);
+}
+
+PreferencesPage::~PreferencesPage() {}
