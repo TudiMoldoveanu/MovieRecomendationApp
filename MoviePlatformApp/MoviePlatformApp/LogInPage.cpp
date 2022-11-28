9a069ec832de63782ebbc942b83a91d535740ef0
TudiMoldoveanu <tudimoldoveanu@gmail.com>
2022-11-28 13:45:22 +0200

    For the button that has an eye icon on the login page, made a functionality that displays password typed into the proper line edit.

--- MoviePlatformApp/MoviePlatformApp/LogInPage.cpp
+++ MoviePlatformApp/MoviePlatformApp/LogInPage.cpp
@@ -6,6 +6,7 @@ LogInPage::LogInPage(QWidget *parent)
 {
 
     ui.setupUi(this);
+    connect(ui.pushButton, SIGNAL(clicked()), SLOT(on_pushButton_clicked()));
 }
 
 LogInPage::~LogInPage()
@@ -23,15 +24,29 @@ bool LogInPage::verifiyLogin(std::string username, std::string password)
     return false;
 }
 
-void LogInPage::on_logInButton_clicked() {
+
+void LogInPage::on_logInButton_clicked()
+{
     std::string username = ui.userLineEdit->text().toStdString();
     std::string  password = ui.passLineEdit->text().toStdString();
 
     if (verifiyLogin(username, password)) {
         QMessageBox::information(this, "Login", "Logat cu succes");
     }
-
-    //TODO: add else case in where the pass and username doesn't match
-
-    //TODO: add functionality on register button
 }
+
+void LogInPage::on_registerButton_clicked()
+{
+    registerPage = new RegisterPage(this);
+    registerPage->show();
+    
+}
+
+void LogInPage::on_pushButton_clicked()
+{
+        QString pass = ui.passLineEdit->text();
+        ui.passLineEdit->setEchoMode(QLineEdit::EchoMode::Normal);
+         ui.passLineEdit->setText(pass); 
+}
+
+//TODO: add else case in where the pass and username doesn't match
