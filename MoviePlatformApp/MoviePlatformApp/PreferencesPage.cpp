cf89c84ddf99172f9575d6bd698181fa43bede49
raluca47 <raluca987123@gmail.com>
2022-12-11 02:24:00 +0200

    Finalized method to fill a row with movie posters

--- MoviePlatformApp/MoviePlatformApp/PreferencesPage.cpp
+++ MoviePlatformApp/MoviePlatformApp/PreferencesPage.cpp
@@ -14,3 +14,46 @@ PreferencesPage::PreferencesPage(QWidget* parent)
 }
 
 PreferencesPage::~PreferencesPage() {}
+
+void PreferencesPage::setMovieData(const int& tableLine, const int& movieId, QStandardItemModel* tableData)
+{
+    const QSize btnSize = QSize(140, 92);
+    for (int col = 0; col < tableSize; col++)
+    {
+        QStandardItem* movieData = new QStandardItem();
+        int random = 1 + (rand() % 3000);
+        QPixmap moviePoster = getMoviePoster(random, "92");
+        movieData->setData(QVariant(moviePoster), Qt::DecorationRole);
+        tableData->setItem(col, tableLine, movieData);
+    }
+}
+
+QPixmap PreferencesPage::getMoviePoster(const int& id, const std::string& size) {
+
+    Movie movie = database->getById<Movie>(id);
+
+    std::string moviePosterUrl = std::move(*movie.getPosterUrl());
+    std::string movieTitle = std::move(*movie.getPosterUrl());
+
+    QUrl requestUrl;
+
+    QString finalUrl = "https://image.tmdb.org/t/p/w" + QString::fromStdString(size) + QString::fromStdString(moviePosterUrl);
+    requestUrl = QUrl(finalUrl);
+
+    return downloadMoviePoster(requestUrl);
+}
+
+QPixmap PreferencesPage::downloadMoviePoster(QUrl url) {
+
+    QNetworkAccessManager network;
+    QEventLoop loop;
+    QObject::connect(&network, &QNetworkAccessManager::finished, &loop, &QEventLoop::quit);
+    QNetworkReply* reply = network.get(QNetworkRequest(url));
+    loop.exec();
+
+    QPixmap moviePoster;
+    moviePoster.loadFromData(reply->readAll());
+
+    delete reply;
+    return moviePoster;
+}
