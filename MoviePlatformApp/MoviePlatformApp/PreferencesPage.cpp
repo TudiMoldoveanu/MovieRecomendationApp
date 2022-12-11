512df0e075226f020f9e19d99e704bfdce219ad3
raluca47 <raluca987123@gmail.com>
2022-12-11 02:19:38 +0200

    Defined members that i'll use to add movie posters for users to select

--- MoviePlatformApp/MoviePlatformApp/PreferencesPage.cpp
+++ MoviePlatformApp/MoviePlatformApp/PreferencesPage.cpp
@@ -1,9 +1,16 @@
 #include "PreferencesPage.h"
+#include "LogInPage.h"
+#include "Movie.h"
+#include<QSize>
 
+const int tableSize = 7;
 PreferencesPage::PreferencesPage(QWidget* parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    m_allMovies = database->getAll<Movie>();
+    m_moviePosters = new QStandardItemModel();
+    setMoviePosters(0, tableSize);
 }
 
 PreferencesPage::~PreferencesPage() {}
